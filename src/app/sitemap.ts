92cf39cad3607194fa4a52ea80121ecd38e2128b
Muslim <81385968+muslimon@users.noreply.github.com>
2024-06-22 18:49:17 +0300

    sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,44 +1,51 @@
-// import { MetadataRoute } from "next";
-// import { createClient } from "@/prismicio";
+import { MetadataRoute } from "next";
+import { createClient } from "@/prismicio";
 
-// type Params = { uid: string };
+type Params = { uid: string };
 
 
-// export default async function sitemap({ params }: { params: Params }): Promise<MetadataRoute.Sitemap> {
-//     const client = createClient();
-//     const page = await client
-//       .getByUID("catalog_category", params.uid)
-     
-//     return [
-//         {
-//             url: "https://mebelmassive.com",
-//             lastModified: new Date(),
-//             changeFrequency: "monthly",
-//             priority: 1,
-//         },
-//         {
-//             url: "https://mebelmassive.com/contacts",
-//             lastModified: new Date(),
-//             changeFrequency: "monthly",
-//             priority: 0.9,
-//         },
-//         {
-//             url: "https://mebelmassive.com/catalog",
-//             lastModified: new Date(),
-//             changeFrequency: "weekly",
-//             priority: 1,
-//         },
-//         {
-//             url: "https://mebelmassive.com/about_us",
-//             lastModified: new Date(),
-//             changeFrequency: "monthly",
-//             priority: 0.7,
-//         },
-//         {
-//             url: "https://mebelmassive.com/works",
-//             lastModified: new Date(),
-//             changeFrequency: "weekly",
-//             priority: 0.8,
-//         },
-//     ];
-// }
+export default async function sitemap({ params }: { params: Params }): Promise<MetadataRoute.Sitemap> {
+    const client = createClient();
+    const pages = await client.getAllByType("catalog_category")
+
+    const categoryEntries:MetadataRoute.Sitemap = pages.map(({ uid }) => ({
+        url: `https://mebelmassive.com/catalog/${uid}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 0.8,
+    }))
+    
+    return [
+        {
+            url: "https://mebelmassive.com",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 1,
+        },
+        {
+            url: "https://mebelmassive.com/contacts",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.9,
+        },
+        {
+            url: "https://mebelmassive.com/catalog",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+        {
+            url: "https://mebelmassive.com/about_us",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.7,
+        },
+        {
+            url: "https://mebelmassive.com/works",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        ...categoryEntries
+    ];
+}
